55c1421b2b7c75b4705b5bcd1200e11c65540be9
FlorianCogoni <floco10@hotmail.fr>
2017-10-21 23:08:55 +0200

    images

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='Jam_Espionnage',
+    version='',
+    packages=['cocos', 'cocos.audio', 'cocos.audio.SDL', 'cocos.audio.pygame', 'cocos.layer', 'cocos.scenes',
+              'cocos.actions', 'cocos2d-0.6.5.cocos2d-0.6.5.build.lib.linux-x86_64-2.7.cocos',
+              'cocos2d-0.6.5.cocos2d-0.6.5.build.lib.linux-x86_64-2.7.cocos.audio',
+              'cocos2d-0.6.5.cocos2d-0.6.5.build.lib.linux-x86_64-2.7.cocos.audio.SDL',
+              'cocos2d-0.6.5.cocos2d-0.6.5.build.lib.linux-x86_64-2.7.cocos.audio.pygame',
+              'cocos2d-0.6.5.cocos2d-0.6.5.build.lib.linux-x86_64-2.7.cocos.layer',
+              'cocos2d-0.6.5.cocos2d-0.6.5.build.lib.linux-x86_64-2.7.cocos.scenes',
+              'cocos2d-0.6.5.cocos2d-0.6.5.build.lib.linux-x86_64-2.7.cocos.actions',
+              'cocos2d-0.6.5.cocos2d-0.6.5.utest.pyglet_mockup1.pyglet',
+              'cocos2d-0.6.5.cocos2d-0.6.5.utest.pyglet_mockup1.pyglet.gl',
+              'cocos2d-0.6.5.cocos2d-0.6.5.utest.pyglet_mockup1.pyglet.gl.glext_arb',
+              'cocos2d-0.6.5.cocos2d-0.6.5.utest.pyglet_mockup1.pyglet.app',
+              'cocos2d-0.6.5.cocos2d-0.6.5.utest.pyglet_mockup1.pyglet.text',
+              'cocos2d-0.6.5.cocos2d-0.6.5.utest.pyglet_mockup1.pyglet.text.formats',
+              'cocos2d-0.6.5.cocos2d-0.6.5.utest.pyglet_mockup1.pyglet.image',
+              'cocos2d-0.6.5.cocos2d-0.6.5.utest.pyglet_mockup1.pyglet.window',
+              'cocos2d-0.6.5.cocos2d-0.6.5.utest.pyglet_mockup1.pyglet.graphics',
+              'cocos2d-0.6.5.cocos2d-0.6.5.utest.pyglet_mockup1.pyglet.resource'],
+    url='',
+    license='',
+    author='florian',
+    author_email='',
+    description=''
+)
